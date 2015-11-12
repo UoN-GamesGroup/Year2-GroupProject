898bb5d0938f17ddef5f3a1a01054c738372cd3b
Matster2 <matster246@gmail.com>
2015-11-12 23:18:24 +0000

    Exterior - Player Movement

---
+++ Year2GroupProject/Assets/Exterior/Scripts/ExteriorPlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExteriorPlayerController : MonoBehaviour {
+
+	//Temporary Controls: Will Change to Assigned Keys Maps and Add Controller Support Later
+
+	public GameObject Gun;
+
+	double dY = 0;
+	float fY = 0;
+	
+	double dZ = 0;
+	float fZ = 0;
+
+	void Start () {
+	
+	}
+
+	void FixedUpdate () {
+		transform.rotation = Quaternion.Euler (0f, fY, 0f);
+		Gun.transform.rotation = Quaternion.Euler (0f, fY, fZ);
+
+		if (Input.GetKey (KeyCode.W)) {
+			dZ += 1;
+			fZ = (float)dZ;
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			dZ -= 1;
+			fZ = (float)dZ;			
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			dY -= 1;
+			fY = (float)dY;
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			dY += 1;
+			fY = (float)dY;			
+		}
+	}
+}
