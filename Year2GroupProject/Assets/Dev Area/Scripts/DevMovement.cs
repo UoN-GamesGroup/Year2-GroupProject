c79a366e30bbb792383fde13836de3e193621080
Matster2 <matster246@gmail.com>
2015-11-27 13:15:11 +0000

    Stress Test Scene

---
+++ Year2GroupProject/Assets/Dev Area/Scripts/DevMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DevMovement : MonoBehaviour {
+
+	public float MovementSpeed = 5f;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.W)){
+			transform.localPosition += transform.forward * MovementSpeed * Time.deltaTime;  
+		}
+		//MoveLeft
+		if (Input.GetKey(KeyCode.A)){
+			transform.localPosition -= transform.right * MovementSpeed * Time.deltaTime;  
+		}
+		//MoveBack
+		if (Input.GetKey(KeyCode.S)){
+			transform.localPosition -= transform.forward * MovementSpeed * Time.deltaTime;  
+		}
+		//MoveRight
+		if (Input.GetKey(KeyCode.D)){
+			transform.localPosition += transform.right * MovementSpeed * Time.deltaTime;  
+		}
+	}
+}
---
+++ Year2GroupProject/Assets/Dev Area/Scripts/DevStressSpawning.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DevStressSpawning : MonoBehaviour {
+
+	public GameObject spawnObject;
+	public Text counterLabel;
+
+	int counter = 0;
+	// Update is called once per frame
+	void Start () {
+		InvokeRepeating ("spawn", 1, 1);
+	}
+
+	void spawn(){
+		Instantiate (spawnObject, transform.position, transform.rotation);
+		counter++;
+		counterLabel.text = "Total Spawned: " + counter;
+	}
+}
