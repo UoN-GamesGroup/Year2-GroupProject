a1dc5099ffbeb9b7f1b3ae50c5c24e8aa4198637
Matster2 <matster246@gmail.com>
2016-02-03 14:00:53 +0000

    Interior Level Base Environment

---
+++ Year2GroupProject/Assets/User Interface/Scripts/LoadingIcon.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadingIcon : MonoBehaviour {
+
+	public float rotationSpeed;
+
+	void FixedUpdate () {
+		transform.Rotate (0, rotationSpeed, 0);
+	}
+}
