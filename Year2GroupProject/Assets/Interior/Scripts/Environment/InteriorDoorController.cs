8e874f03289d84fdba0705e733108a0d7918ec55
Matster2 <matster246@gmail.com>
2016-02-03 22:37:24 +0000

    Interior Door Animations and Triggers

---
+++ Year2GroupProject/Assets/Interior/Scripts/Environment/InteriorDoorController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteriorDoorController : MonoBehaviour {
+
+	Animator animator;
+
+	void Start(){
+		animator = this.gameObject.GetComponent<Animator> ();
+	}
+	void OnTriggerEnter(Collider collider){
+		if (collider.gameObject.CompareTag("Player")){
+			animator.SetBool ("Activated", true);
+		}
+	}
+
+	void OnTriggerExit(Collider collider){
+		if (collider.gameObject.CompareTag("Player")){
+			animator.SetBool ("Activated", false);
+		}
+	}
+}
